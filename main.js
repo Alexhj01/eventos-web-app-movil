3f17c2d093f3d527dc2f9fa0027f2ada754c1b32
Alex JimeneZ <est.aherreraj0201>
2023-04-15 12:33:02 -0600

    Modificaciones

---
+++ main.js
@@ -0,0 +1,14 @@
+//Cargar los estilos de la aplicación
+import './src/css/style.css';
+
+//Activar el plugin de flowbite
+import "flowbite"
+
+//Cargar la barra de navegacion
+import './src/js/barra.js';
+
+//Cargar el carrucel
+import './src/js/slider.js';
+
+//Cargar el header
+import './src/js/header.js'
--- src/js/main.js
+++
@@ -1,5 +0,0 @@
-//Cargar los estilos de la aplicación
-import '/src/css/style.css';
-
-//Activar el plugin de flowbite
-import "flowbite"
